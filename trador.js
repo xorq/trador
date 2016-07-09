0e77cd3b780a05a386d54772fce22bb2d87a4d0e
Daniel Garniron <dandan@Daniels-MacBook-Pro.local>
2016-07-09 18:55:18 +0700

    function update THBUSD

--- trador.js
+++ trador.js
@@ -1,3 +1,10 @@
+var updateTHBUSD = function(){
+	$.getJSON('http://apilayer.net/api/live?access_key=c60f3e8c41a9313bc52f1279d9fa9cb6&currencies=THB&source=USD&format=1', function(a){
+		window.localStorage.setItem('THBUSD', a.quotes.USDTHB);
+	})
+	
+}
+
 var findAskVolume = function(a){
 	var cumulater = _.map(a, function(b){
 		return b
@@ -11,7 +18,11 @@ var a = function(){
 	})
 };
 
+var afterUpdate = function(a){
+	console.log(a)
+}
+
 a().done(function(a){
-	console.log(findAskVolume(a.asks))
+	afterUpdate(a);
 });
 
