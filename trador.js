c9c5ec617b696c361ad1e273ac5917a3964ec85e
Daniel Garniron <dandan@Daniels-MacBook-Pro.local>
2016-07-11 08:30:10 +0700

    quotation object getBXBestPrice

--- trador.js
+++ trador.js
@@ -44,6 +44,17 @@ var quotation = Backbone.Model.extend({
 		return $.getJSON('/bxorderbook', function(a, b){
 			master.set('BXOrderbook', a);
 		})
+	},
+	getBXBest : function(bidAsk, THBUSD){
+		var master = this;
+		var rate = THBUSD == 'USD' ? master.get('USDTHBRate') : THBUSD == 'THB' ? 1 : null;
+		if (rate == null) {
+			throw 'you must specify a currency'
+		}
+		return {
+			Bprice: master.get('BXOrderbook')[bidAsk][0][0] * 1 / rate,
+			Bquantity: master.get('BXOrderbook')[bidAsk][0][1]
+		}
 	}
 })
 
