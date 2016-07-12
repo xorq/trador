9a4b443e9c24bdba70f8ae767810e80658e78e44
Daniel Garniron <dandan@Daniels-MacBook-Pro.local>
2016-07-12 17:19:00 +0700

    cleaning

--- trador.js
+++ trador.js
@@ -21,66 +21,71 @@ var afterUpdate = function(a){
 
 var quotation = Backbone.Model.extend({
 	defaults: {
-		USDTHBRate : null,
+		rates : null,
 		BXOrderbook: null,
 		BFXOrderbook: null
 	},
 	initialize : function(){
 		//On production change this to : this.updateRate();
-		this.set('USDTHBRate', 35.16);
+		this.set('rates', {usd: 1, thb: 35.16});
 		this.updateBX();
 		this.updateBFX();
 	},
 	updateRate : function(){
 		var master = this;
 		updateTHBUSD().done(function(a){
-			master.set('USDTHBRate', a.quotes.USDTHB)
+			var newRates = master.get('rates');
+			newRates.thb = a.quotes.USDTHB;
+			master.set('rates', rates);
 		})
 	},
 	updateBX : function(){
 		var master = this;
 		return $.getJSON('/bxorderbook', function(a, b){
 			master.set('BXOrderbook', a);
-			return true
 		})
 	},
 	updateBFX : function(){
 		var master = this;
 		return $.getJSON('/bfxorderbook', function(a, b){
 			master.set('BFXOrderbook', a)
-			return true
 		})
 	},
-	getBXBest : function(bidAsk, THBUSD){
+	getBXBest : function(bidAsk, currency){
 		var master = this;
-		var rate = THBUSD == 'USD' ? master.get('USDTHBRate') : THBUSD == 'THB' ? 1 : null;
+		var rate = currency == 'USD' ? master.get('rates').thb : currency == 'THB' ? 1 : null;
 		if (rate == null) {
 			throw 'you must specify a currency'
 		}
+		console.log(rate)
+		console.log(master.get('BXOrderbook')[bidAsk][0][0])
 		return {
-			Bprice: master.get('BXOrderbook')[bidAsk][0][0] * 1 / rate,
+			Bprice: master.get('BXOrderbook')[bidAsk][0][0] / rate,
 			Bquantity: master.get('BXOrderbook')[bidAsk][0][1]
 		}
 	},
 	getBFXBest: function(bidAsk){
 		var master = this;
 		return {
-			Bprice: master.get('BFXOrderbook')[bidAsk][0].price,
-			Bquantity: master.get('BFXOrderbook')[bidAsk][0].amount
+			Bprice: master.get('BFXOrderbook')[bidAsk][0].price * 1,
+			Bquantity: master.get('BFXOrderbook')[bidAsk][0].amount * 1
 		}
 	},
 	opportunityBXBFX: function(){
 		return {
 			oppBuyBX : Math.round(10 * (this.getBFXBest('bids').Bprice - this.getBXBest('asks', 'USD').Bprice)) / 10,
-			oppBuyBFX : Math.round(10 * (this.getBXBest('bids', 'USD').Bprice - this.getBFXBest('asks').Bprice)) / 10
+			oppBuyBFX : Math.round(10 * (this.getBXBest('bids', 'USD').Bprice - this.getBFXBest('asks').Bprice)) / 10,
+			buyBXat : this.getBXBest('asks', 'THB').Bprice,
+			sellBFXat : this.getBFXBest('bids', 'USD').Bprice,
+			buyBFXat : this.getBFXBest('asks', 'USD').Bprice,
+			sellBFXat : this.getBXBest('bids', 'THB').Bprice
 		}
 	},
 	refreshOpp: function(){
 		var master = this;
 		master.updateBX().done(function(){
 			console.log('bx updated')
-			master.updateBFX().done(function(){
-				console.log('bfx updated');
+			master.updateBFX().done(function(a){
 				console.log(master.opportunityBXBFX())
 				return true
 			})
